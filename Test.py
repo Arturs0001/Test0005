69bdbde60db136566e279d7284632f07014601f4
Artur <ksks@gmail.com>
2024-02-24 17:39:26 +0200

    2.0

--- Test.py
+++ Test.py
@@ -1,10 +1,7 @@
 import requests
-
-import lxml
-
 from bs4 import BeautifulSoup
 
-url = "https://kups.club"
+url_base = "https://kups.club"
 header = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36"}
 
 session = requests.session()
@@ -13,21 +10,26 @@ for j in range(1, 10):
     print(f"PAGE = {j}")
     url = f"https://kups.club/?page={j}"
 
+    response = session.get(url, headers=header)
 
-    responce = session.get(url, headers=header)
-    # print(responce)
-    if responce.status_code == 200:
-        soup = BeautifulSoup(responce.text, "lxml")
+    if response.status_code == 200:
+        soup = BeautifulSoup(response.text, "lxml")
         allProduct = soup.find("div", class_="row mt-4")
         products = allProduct.find_all("div", class_="card h-100")
 
         for i in range(len(products)):
+            title = None
+            img = None
+            price = None
+
             try:
                 title = products[i].find("a", class_='text-black link-default').text
-                img = products[i].find("img", class_='mr-2').text
                 price = products[i].find("p", class_='card-text').text
-                with open("videogamesaturn.txt", "a", encoding="UTF-8") as file:
-                    file.write(f"{title} {img}---> {price}\n")
             except:
-                print(f"{title} скидки неть")
-                print(f"{img} скидки неть")
+                img = products[i].find("img", class_='mr-2')
+                print("Произошла ошибка, но нет информации о её типе")
+                print(f"{title} скидки нет")
+                print(f"{img} скидки нет")
+
+            with open("videogamesaturn.txt", "a", encoding="UTF-8") as file:
+                file.write(f"{title} {img}---> {price}\n")
